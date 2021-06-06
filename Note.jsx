7f104ade45bf5d2a1540ee56c604b08f792eb890
Prashanthi0110 <78533766+Prashanthi0110@users.noreply.github.com>
2021-06-06 15:12:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        7 day long free Web-Dev with Javascript and React.js Bootcamp.This
+        Bootcamp will be taken by Shaurya Sinha DATA ANALYST intern at JIO. You
+        can learn Html,Javascript ,React.js.
+      </p>
+    </div>
+  );
+}
+export default Note;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../public/style.css";
+import App from "./components/App";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
